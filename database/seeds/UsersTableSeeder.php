b70102daebd827edba490115bb39cd6ac2c6cd68
Ibn <ibn_ali@yahoo.com>
2019-11-07 14:57:00 +0100

    usersSeeds_changed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create(
+            [
+                'name'=> 'admin',
+                'email'=> 'admin@admin.com',
+                'password'=> bcrypt('12345678'),
+            ]
+        );
+
+        App\User::create(
+            [
+                'name'=> 'master',
+                'email'=> 'master@master.com',
+                'password'=>bcrypt('12345678'),
+            ]
+        );
+
+        App\User::create(
+            [
+                'name'=>'ibn',
+                'email'=>'ibn.ali@gmx.fr',
+                'password'=>bcrypt('12345678'),
+            ]
+        );
+    }
+}
