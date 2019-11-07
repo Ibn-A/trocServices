678a7b2e2cbf470254f4947f0808d6d36a57aa60
Ibn <ibn_ali@yahoo.com>
2019-11-07 16:15:52 +0100

    creation_offreServiceModel&Migration

---
+++ database/migrations/2019_11_07_141353_create_offre_services_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffreServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offre_services', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->text('contenu_offre', 300);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offre_services');
+    }
+}
