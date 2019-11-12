7aef6fbdb460e25d57e1ae0f0abb27c131cc0c1c
admin <admin@MacBook-Pro-de-Elsa.local>
2019-11-12 01:08:24 +0100

    LocalisationModel&relationships_setup

--- app/Localisation.php
+++ app/Localisation.php
@@ -9,4 +9,19 @@ class Localisation extends Model
     protected $fillable = [
         'adresse_localisation','cp_localisation','ville_localisation'
     ];
+    /**
+     * Obtenir les demandes de services d'une localisation.
+     */
+    public function demandeServices()
+    {
+        $this->hasMany('App\DemandeService');
+    }
+    /**
+     * Obtenir les offres de services d'une localisation.
+     */
+    public function offreServices()
+    {
+        return $this->hasMany('App\OffreService');
+    }
+
 }
