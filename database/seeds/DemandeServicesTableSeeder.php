3acc2ac685aa79c93ef66a51c62f324b6d206613
Ibn <ibn_ali@yahoo.com>
2019-11-14 15:05:24 +0100

    DemandeServicesTableSeeder_update(foreign keys)

--- database/seeds/DemandeServicesTableSeeder.php
+++ database/seeds/DemandeServicesTableSeeder.php
@@ -16,6 +16,9 @@ class DemandeServicesTableSeeder extends Seeder
                 'objet_demande'=>'recherche massage thailandais',
                 'contenu_demande'=>'Je débute en entraienement intensif en boxe thai et,
                  je souhaiterais avoir  des massage thailandais fait par un spécialiste pour cet été.',
+                'user_id'=> 1,
+                'service_id'=> 1,
+                'localisation_id'=> 1
             ]
         );
     }
