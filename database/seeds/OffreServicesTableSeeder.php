a7e19441b686d268f7002ad4dd55e7b974dd80d0
Ibn <ibn_ali@yahoo.com>
2019-11-14 15:06:10 +0100

    OffreServicesTableSeeder_update(foreign keys)

--- database/seeds/OffreServicesTableSeeder.php
+++ database/seeds/OffreServicesTableSeeder.php
@@ -16,6 +16,9 @@ class OffreServicesTableSeeder extends Seeder
                 'objet_offre'=>'propose 1h de massage thailandais',
                 'contenu_offre'=>'Expert en massage thailandais,
                  je peux vous offrir des services de massages thai pour vous détentre et relaxer vos muscles et vos articulations.',
+                'user_id'=> 1,
+                'service_id'=> 1,
+                'localisation_id'=> 1
             ]
         );
     }
