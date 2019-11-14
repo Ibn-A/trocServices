70c77834257e26ddc058e2d7e741439b34f46a51
Ibn <ibn_ali@yahoo.com>
2019-11-14 15:07:59 +0100

    DemandeServicesSeeder_update(contenu_demande)

--- database/seeds/DemandeServicesTableSeeder.php
+++ database/seeds/DemandeServicesTableSeeder.php
@@ -14,7 +14,7 @@ class DemandeServicesTableSeeder extends Seeder
         App\DemandeService::create(
             [
                 'objet_demande'=>'recherche massage thailandais',
-                'contenu_demande'=>'Je débute en entraienement intensif en boxe thai et,
+                'contenu_demande'=>'Je débute un entraienement intensif en boxe thai et,
                  je souhaiterais avoir  des massage thailandais fait par un spécialiste pour cet été.',
                 'user_id'=> 1,
                 'service_id'=> 1,
