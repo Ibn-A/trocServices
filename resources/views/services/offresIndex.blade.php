92f7bf3ff8dbd8cbafe92a3a59b7a8a822545ace
Ibn <ibn_ali@yahoo.com>
2019-11-25 16:54:45 +0100

    offresIndex.blade_update

---
+++ resources/views/services/offresIndex.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.template')
+@section('content')
+<section class="section">
+    <div class="container has-text-centered">
+        <h1 class="title">Les services </h1>
+
+        <div class="tile is-ancestor divider my-3">
+            @foreach($services as $service) 
+            <div class="tile is-vertical is-parent ">
+                <article class="tile is-child box">
+                    <figure class="image">
+                        <img src="">
+                    </figure>
+                    <p class="title">{{ $service->nomService }}</p>
+                        <tr>
+                                <td><a class="button is-warning" href="{{ route('services.index') }}">Voir</a></td>
+                            
+                        </tr>
+                        
+                    </article>
+            </div>
+            @endforeach  
+        </div>
+    </div>
+</section>
+@endsection
