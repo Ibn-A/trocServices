d3db8dabc02404eb2a9b778b75f36c9e3540e206
Ibn-A <ibn_ali@yahoo.com>
2019-11-26 08:43:51 +0100

    accueil_update

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -23,7 +23,7 @@
 
         <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
+                <img src="/img/solidR.svg" width="40" height="30" class="d-inline-block align-top" alt="">
                 trocServices
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
