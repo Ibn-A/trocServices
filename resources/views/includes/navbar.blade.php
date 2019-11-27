e7a84428990c2a83dbefc4a4bde980eb16ad5ca1
Ibn-A <ibn_ali@yahoo.com>
2019-11-27 08:51:22 +0100

    navbar_update

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -39,11 +39,13 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class ="dropdown-item" href=""> {{__('Profil')}}</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Se deconnecter') }}
                                     </a>
+                                    
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
