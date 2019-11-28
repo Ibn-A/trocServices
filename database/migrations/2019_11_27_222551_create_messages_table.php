f120146ff6359682b8756b94510ad15643beeb16
Ibn-A <ibn_ali@yahoo.com>
2019-11-28 14:00:19 +0100

    message_migrationsTable_setup

---
+++ database/migrations/2019_11_27_222551_create_messages_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('texte');
+            $table->string('email');
+            $table->timestamps();
+            
+            $table->unsignedBigInteger('offreService_id');
+            $table->foreign('offreService_id')
+                ->references('id')
+                ->on('offre_services')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->unsignedBigInteger('demandeService_id');
+            $table->foreign('demandeService_id')
+                ->references('id')
+                ->on('demande_services')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
