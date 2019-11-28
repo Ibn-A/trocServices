d72f525e4c65d6ff4766ee73881fa6839f0dca7e
Ibn-A <ibn_ali@yahoo.com>
2019-11-28 13:59:00 +0100

    offreService_factories_setup

---
+++ database/factories/OffreService.php
@@ -0,0 +1,44 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Carbon\Carbon;
+use App\Localisation;
+use Faker\Generator as Faker;
+
+function getRandGeo($url)
+{
+    $results = file_get_contents($url);
+    $elements = json_decode($results, true);
+    $id = rand(0, count($elements) - 1);
+    return $elements[$id];
+}
+
+$factory->define(Model::class, function (Faker $faker) {
+
+    $localisation_id = rand(1, 12);
+    $localisation_code = Localisation::find($localisation_id)->code;
+    $departement = getRandGeo('https://geo.api.gouv.fr/regions/' . $localisation_code . '/departements');
+    $commune = getRandGeo('https://geo.api.gouv.fr/departements/' . $departement['code'] . '/communes');
+    $obsolete = rand(0, 1);
+
+    return [
+
+        'titre' => $faker->sentence($nbWords = 3, $variableNbWords = true),
+        'texte' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+        'service_id' => rand(1, 10),
+        'localisation_id' => $localisation_id,
+        'user_id' => rand(2, 3),
+        'departement' => $departement['code'],
+        'commune' => $commune['code'],
+        'commune_name' => $commune['nom'],
+        'commune_postal' => $commune['codesPostaux'][0],
+        'pseudo' => $faker->word,
+        'email' => $faker->email,
+        'limit' => $obsolete ? Carbon::now()->subDayss(rand(1, 30)) : Carbon::now()->addWeeks(rand(1, 4)),
+        'active' => rand(0, 1),
+
+        
+    ];
+});
