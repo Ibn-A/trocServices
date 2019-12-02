f2ac9fc6aa79f4140f2da5a5b44e8b07daab76d3
Ibn-A <ibn_ali@yahoo.com>
2019-12-02 09:15:48 +0100

    OffreStore_Request

---
+++ app/Http/Requests/OffreStore.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OffreStore extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'string', 'max:100'],
+            'texte' => ['required', 'string', 'max:1000'],
+            'pseudo' => ['required', 'string', 'max:20'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+        ];
+    }
+}
