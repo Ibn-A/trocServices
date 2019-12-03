c6cf55fc06994befa4c7a03bb3baf53f5f03e559
Ibn <ibn_ali@yahoo.com>
2019-12-03 13:40:36 +0100

    footer_setup

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,7 @@
+
+<nav class="navbar navbar-expand fixed-bottom navbar-dark">
+    <div class="navbar-nav ml-auto">
+        <a class="nav-item nav-link " href="">Mentions légales</a>
+        <a class="nav-item nav-link " href="">Politique de confidentialité</a>
+    </div>
+</nav>
