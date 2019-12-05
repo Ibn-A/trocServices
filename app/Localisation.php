58e64a2f554a8d3bf153785c4a2e41aac946cc45
IBN <ibnali@MacBook-Air-de-IBN.local>
2019-12-05 16:14:43 +0100

    LocalisationModel_update

--- app/Localisation.php
+++ app/Localisation.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Localisation extends Model
 {
     protected $fillable = [
-        'nom', 'slug','code'
+        'name', 'slug','code'
     ];
     /**
      * Obtenir les demandes de services d'une localisation.
